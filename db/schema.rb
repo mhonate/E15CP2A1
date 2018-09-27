a2539e44349d05d482daa8654b36d6f099276067
Marcelo <marcelo.onate@gmail.com>
2018-09-26 21:58:02 -0300

    add hasta agregar strong params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180926183121) do
+ActiveRecord::Schema.define(version: 20180927000750) do
 
   create_table "histories", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180926183121) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +35,9 @@ ActiveRecord::Schema.define(version: 20180926183121) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
